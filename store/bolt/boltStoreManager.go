6ffd0d199c660ec6082f726fd5b475c2cb6701aa
= <=>
2024-11-16 17:59:56 +0100

    Refactor BoltStoreManager to include flux and data buckets
    The data bucket is used as content-addressed storage, where all other buckets store only the SHA-256 digest. This approach deduplicates the data.
    The flux bucket stores the locally received sequence of events.

--- store/bolt/boltStoreManager.go
+++ store/bolt/boltStoreManager.go
@@ -2,7 +2,9 @@ package bolt
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"iter"
 
 	"github.com/google/uuid"
 	"github.com/hallgren/eventsourcing/core"
@@ -13,11 +15,13 @@ import (
 
 type BoltStoreManager struct {
 	db          *bbolt.DB
-	onCommitCbs []func(fluxcore.SubStore, []core.Event)
+	onCommitCbs []func(fluxcore.SubStore, []fluxcore.Event)
 }
 
 const (
 	globalStoreBucketName = "global_store"
+	fluxBucketName        = "flux"
+	dataBucketName        = "data"
 )
 
 // NewBoltStoreManager creates a new EdgeStoreManager
@@ -32,6 +36,16 @@ func NewBoltStoreManager(path string) (*BoltStoreManager, error) {
 		if err != nil {
 			return err
 		}
+
+		_, err = tx.CreateBucketIfNotExists([]byte(fluxBucketName))
+		if err != nil {
+			return err
+		}
+
+		_, err = tx.CreateBucketIfNotExists([]byte(dataBucketName))
+		if err != nil {
+			return err
+		}
 		return nil
 	})
 	if err != nil {
@@ -40,13 +54,12 @@ func NewBoltStoreManager(path string) (*BoltStoreManager, error) {
 
 	return &BoltStoreManager{
 		db:          db,
-		onCommitCbs: make([]func(fluxcore.SubStore, []core.Event), 0),
+		onCommitCbs: make([]func(fluxcore.SubStore, []fluxcore.Event), 0),
 	}, nil
 }
 
-func (m *BoltStoreManager) List(metadata map[string]string) <-chan fluxcore.SubStore {
-	ch := make(chan fluxcore.SubStore)
-	go func() {
+func (m *BoltStoreManager) List(metadata map[string]string) iter.Seq[fluxcore.SubStore] {
+	return func(yield func(fluxcore.SubStore) bool) {
 		m.db.View(func(tx *bbolt.Tx) error {
 			b := tx.Bucket([]byte(globalStoreBucketName))
 			if b == nil {
@@ -69,18 +82,20 @@ func (m *BoltStoreManager) List(metadata map[string]string) <-chan fluxcore.SubS
 					}
 				}
 
-				ch <- &BoltSubStore{
+				subStore := &BoltSubStore{
 					manager:  m,
 					db:       m.db,
 					id:       fluxcore.StoreId(id),
 					metadata: loadedMetadata,
 				}
+
+				if !yield(subStore) {
+					return nil
+				}
 				return nil
 			})
 		})
-		close(ch)
-	}()
-	return ch
+	}
 }
 
 func (m *BoltStoreManager) Create(id fluxcore.StoreId, metadata map[string]string) (fluxcore.SubStore, error) {
@@ -130,7 +145,86 @@ func (m *BoltStoreManager) Get(id fluxcore.StoreId) (fluxcore.SubStore, error) {
 	}, nil
 }
 
-func (m *BoltStoreManager) OnCommit(cb func(fluxcore.SubStore, []core.Event)) fluxcore.Unsubscriber {
+func (m *BoltStoreManager) All(start core.Version) (iter.Seq[fluxcore.Event], error) {
+	tx, err := m.db.Begin(false)
+	if err != nil {
+		return nil, err
+	}
+
+	fluxBucket := tx.Bucket([]byte(fluxBucketName))
+	if fluxBucket == nil {
+		tx.Rollback()
+		return func(yield func(fluxcore.Event) bool) {}, errors.New("flux bucket not found")
+	}
+
+	dataBucket := tx.Bucket([]byte(dataBucketName))
+	if dataBucket == nil {
+		tx.Rollback()
+		return func(yield func(fluxcore.Event) bool) {}, errors.New("data bucket not found")
+	}
+
+	globalStoreBucket := tx.Bucket([]byte(globalStoreBucketName))
+	if globalStoreBucket == nil {
+		tx.Rollback()
+		return func(yield func(fluxcore.Event) bool) {}, errors.New("global store bucket not found")
+	}
+
+	return func(yield func(fluxcore.Event) bool) {
+		defer tx.Rollback()
+		cursor := fluxBucket.Cursor()
+
+		lookup := map[string]map[string]string{}
+
+		for k, digest := cursor.Seek(itob(uint64(start))); k != nil; k, digest = cursor.Next() {
+			data := dataBucket.Get(digest)
+			if data == nil {
+				return
+			}
+
+			var boltEvent boltEvent
+			err := json.Unmarshal(data, &boltEvent)
+			if err != nil {
+				return
+			}
+
+			storeId := boltEvent.OwnedByStoreId
+			if _, ok := lookup[storeId]; !ok {
+				data := globalStoreBucket.Get([]byte(storeId))
+				if data == nil {
+					return
+				}
+				metadata := make(map[string]string)
+				err = json.Unmarshal(data, &metadata)
+				if err != nil {
+					return
+				}
+				lookup[storeId] = metadata
+			}
+
+			fluxEvent := fluxcore.Event{
+				StoreId:       fluxcore.StoreId(uuid.MustParse(storeId)),
+				StoreMetadata: lookup[storeId],
+				FluxVersion:   core.Version(boltEvent.FluxVersion),
+				Event: core.Event{
+					AggregateID:   boltEvent.AggregateID,
+					Version:       core.Version(boltEvent.Version),
+					GlobalVersion: core.Version(boltEvent.GlobalVersion),
+					AggregateType: boltEvent.AggregateType,
+					Timestamp:     boltEvent.Timestamp,
+					Reason:        boltEvent.Reason,
+					Data:          boltEvent.Data,
+					Metadata:      boltEvent.Metadata,
+				},
+			}
+
+			if !yield(fluxEvent) {
+				return
+			}
+		}
+	}, nil
+}
+
+func (m *BoltStoreManager) OnCommit(cb func(fluxcore.SubStore, []fluxcore.Event)) fluxcore.Unsubscriber {
 	m.onCommitCbs = append(m.onCommitCbs, cb)
 	return fluxcore.UnsubscribeFunc(func() error {
 		for i, c := range m.onCommitCbs {
@@ -143,7 +237,7 @@ func (m *BoltStoreManager) OnCommit(cb func(fluxcore.SubStore, []core.Event)) fl
 	})
 }
 
-func (m *BoltStoreManager) commited(s fluxcore.SubStore, events []core.Event) error {
+func (m *BoltStoreManager) commited(s fluxcore.SubStore, events []fluxcore.Event) error {
 	for _, cb := range m.onCommitCbs {
 		cb(s, events)
 	}
